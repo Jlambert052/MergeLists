8bc2220602520a437977e30a78e3650a9809f463
Jack Lambert <jackwlambert14@gmail.com>
2023-02-13 13:11:21 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LeetCode;
+Console.WriteLine("Hello, World!");
+
+Solution sol = new();
+
+ListNode listA = new(1);
+ListNode listB = new(2);
+
+sol.MergeTwoLists(listA, listB);
